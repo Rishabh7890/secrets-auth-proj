5edf23c9c2c64bbe2737a7649676dfa18c5c39de
rishabh <rishabh7890@gmail.com>
2019-09-24 19:45:41 -0400

    utilizing bcrypt for salt and hash

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
@@ -57,28 +57,32 @@ app.get("/register", function(req, res) {
 // route to register new users
 app.post("/register", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
-  // create newUser document in db
-  const newUser = new User({
-    email: username,
-    password: password
-  });
-  // save newUser to db
-  newUser.save(function(err) {
-    if (err) {
-      res.json(err);
-    } else {
-      // if no errors render secrets page
-      res.render("secrets");
-    }
+  // use bcrypt to hash entered password with 10 saltRounds
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    // create newUser document in db
+    const newUser = new User({
+      email: username,
+      password: hash
+    });
+    // save newUser to db
+    newUser.save(function(err) {
+      if (err) {
+        res.json(err);
+      } else {
+        // if no errors render secrets page
+        res.render("secrets");
+      }
+    });
   });
 });
 
 // route for users to login
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   // look through db for a user with value entered in email field
   User.findOne({ email: username }, function(err, foundUser) {
@@ -86,11 +90,13 @@ app.post("/login", function(req, res) {
       res.json(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          res.status(401).json("Invalid Email or Password");
-        }
+        // use bcrypt.compare to load hash from db and compare
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          // check if hash for 'password' is === hash in db
+          if(result === true){
+            res.render("secrets");
+          }
+        });
       }
     }
   });
